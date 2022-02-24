b0c864b82262339c1c697f7dee167f7fc3e6e348
Семен Муллаяров <semen41ks@mail.ru>
2022-02-24 18:45:05 +0500

    initial commit three

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,12 +9,21 @@ class SQRServiceTest {
     @Test
     void countSquares() {
         SQRService service = new SQRService();
-        int minimum = 200;
-        int maximum = 400;
-        int expected = 6;
+        int minimum = 100;
+        int maximum = 200;
+        int expected = 5;
         int actual = service.countSquares(minimum, maximum);
         assertEquals(expected, actual);
     }
 
+    @Test
+    void countSquares1() {
+        SQRService service = new SQRService();
+        int minimum = 200;
+        int maximum = 300;
+        int expected = 3;
+        int actual = service.countSquares(minimum, maximum);
+        assertEquals(expected, actual);
+    }
 
 }
